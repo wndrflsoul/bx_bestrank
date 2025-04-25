5b7109271d434a35e3954604e071ceeff1af4319
wndrflsoul <root@cv4690955.novalocal>
2025-04-25 11:14:05 +0300

    update gitignore

--- bitrix/managed_cache/MYSQL/b_option/f1/f15a92b72a84bfe84ce9762bfb6cad5f.php
+++ bitrix/managed_cache/MYSQL/b_option/f1/f15a92b72a84bfe84ce9762bfb6cad5f.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001745564157';
-$dateexpire = '001745567757';
+$datecreate = '001745568242';
+$dateexpire = '001745571842';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:10:{s:15:"~export_uniq_id";s:32:"cc27b7dcfef9af11861390d26a7bebfb";s:20:"~ft_b_calendar_event";s:1:"1";s:26:"eventWithLocationConverted";s:1:"Y";s:8:"forum_id";s:1:"8";s:24:"googleToNewSyncConverted";s:1:"Y";s:15:"needChangeColor";s:1:"N";s:14:"needEventIndex";s:1:"N";s:29:"path_to_type_company_calendar";s:10:"/calendar/";s:23:"queue_last_processed_id";s:1:"2";s:25:"sectionStructureConverted";s:1:"Y";}}}';
 return true;
 ?>
--- bitrix/managed_cache/MYSQL/b_user_field_enum/fe/fe73a9f0e93bff906b017636a42e5e5e.php
+++ bitrix/managed_cache/MYSQL/b_user_field_enum/fe/fe73a9f0e93bff906b017636a42e5e5e.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001745564160';
-$dateexpire = '001745567760';
+$datecreate = '001745568243';
+$dateexpire = '001745571843';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:0:{}}';
 return true;
 ?>
